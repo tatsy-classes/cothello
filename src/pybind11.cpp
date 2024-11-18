1dcab184363e531d0984913ca2261f8db5186d82
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2024-11-18 12:41:42 +0900

    Minor update.

--- src/pybind11.cpp
+++ src/pybind11.cpp
@@ -15,13 +15,15 @@ PYBIND11_MODULE(libcpp, m_cpp) {
     // Player
     auto cls_player = py::class_<Player>(m_cpp, "Player");
     cls_player.def(py::init<int>())
-        .def_property_readonly("name", &Player::str)
+        .def_property_readonly("name", &Player::str, "Player name")
         .def_property_readonly_static("BLACK", [](py::object) { return (int)Player::BLACK; })
         .def_property_readonly_static("WHITE", [](py::object) { return (int)Player::WHITE; })
-        .def_static("as_black", [](py::object) { return Player(Player::BLACK); })
-        .def_static("as_white", [](py::object) { return Player(Player::WHITE); })
-        .def("is_black", &Player::isBlack)
-        .def("is_white", &Player::isWhite)
+        .def_static(
+            "as_black", [](py::object) { return Player(Player::BLACK); }, "Create a black player")
+        .def_static(
+            "as_white", [](py::object) { return Player(Player::WHITE); }, "Create a white player")
+        .def("is_black", &Player::isBlack, "Check if the player is black")
+        .def("is_white", &Player::isWhite, "Check if the player is white")
         .def("__eq__", &Player::operator==)
         .def("__ne__", &Player::operator!=)
         .def("__neg__", [](const Player &self) { return -self; })
@@ -31,12 +33,12 @@ PYBIND11_MODULE(libcpp, m_cpp) {
     // Action
     auto cls_action = py::class_<Action>(m_cpp, "Action");
     cls_action.def(py::init<Player, int, int>())
-        .def("is_pass", &Action::isPass)
+        .def("is_pass", &Action::isPass, "Check if the action is pass")
         .def("to_symbol", &Action::toSymbol)
-        .def_static("make_pass", &Action::makePass)
-        .def_property_readonly("player", &Action::getPlayer)
-        .def_property_readonly("x", &Action::getX)
-        .def_property_readonly("y", &Action::getY)
+        .def_static("make_pass", &Action::makePass, "Create a pass action for player specified", py::arg("player"))
+        .def_property_readonly("player", &Action::getPlayer, "Player of the action")
+        .def_property_readonly("x", &Action::getX, "X coordinate")
+        .def_property_readonly("y", &Action::getY, "Y coordinate")
         .def("__str__", &Action::str)
         .def("__repr__", &Action::repr);
 
@@ -48,12 +50,12 @@ PYBIND11_MODULE(libcpp, m_cpp) {
         .def("is_done", &Env::isDone)
         .def_property_readonly("board", [](const Env &self) { return to_numpy<int, 8, 8>(self.board()); })
         .def("count", [](const Env &self, int player) { return self.count(Player(player)); })
-        .def("is_win", &Env::isWin)
-        .def("is_lose", &Env::isLose)
-        .def("undo", &Env::undo)
-        .def("update", &Env::update)
-        .def("legal_actions", &Env::legalActions)
-        .def_property_readonly("player", &Env::getPlayer)
+        .def("is_win", &Env::isWin, "Check if the player wins", py::arg("player"))
+        .def("is_lose", &Env::isLose, "Check if the player loses", py::arg("player"))
+        .def("undo", &Env::undo, "Undo the last action")
+        .def("update", &Env::update, "Update the environment with the action", py::arg("action"))
+        .def("legal_actions", &Env::legalActions, "Get the list of legal actions")
+        .def_property_readonly("player", &Env::getPlayer, "Current player")
         .def_property_readonly("history_size", &Env::historySize)
         .def_property_readonly("last_action", &Env::getLastAction);
 
